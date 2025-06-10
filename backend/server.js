1d0359b75845033ea23833c224ffc98930e5c4ce
Max lambert <max.lambert429@gmail.com>
2025-06-10 14:35:06 -0600

    Fixing express usage

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv"
 import {neon} from "@neondatabase/serverless"
 const sql = neon(process.env.DATABASE_URL)
 dotenv.config()
-const app = express.Router()
+const app = express()
+app.use(express.json());
 app.listen(3000, () => {
     console.log("Server Running")
 })
