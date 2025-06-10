178b30fd83613f6133d360e5b5a9a3cb0dc3f821
Max lambert <max.lambert429@gmail.com>
2025-06-10 15:04:03 -0600

    Implementing routes

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config()
 const sql = neon(process.env.DATABASE_URL)
 const app = express()
 app.use(express.json());
-app.post("/login", loginRoute)
+app.use("/login", loginRoute)
 app.post("/signup", signupRoute)
 app.listen(3000, () => {
     console.log("Server Running")
