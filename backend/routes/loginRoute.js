990319420b503db6987119323e95844294defb82
Max lambert <max.lambert429@gmail.com>
2025-06-11 13:54:10 -0600

    Testing log in route

--- backend/routes/loginRoute.js
+++ backend/routes/loginRoute.js
@@ -2,12 +2,20 @@ import express from "express"
 import dotenv from "dotenv"
 import bcrypt from "bcrypt"
 import {neon} from "@neondatabase/serverless"
+import checkLogin from "./middleware/checkLogin"
 dotenv.config()
 const sql = neon(process.env.DATABASE_URL)
 const app = express.Router()
 app.use(express.urlencoded({ extended: true }));
 app.post("/", async (req, res) => {
     const {username, password} = req.body
+    const checkLogin = checkLogin(username)
+    if (!checkLogin.success) {
+        req.redirect("/login")
+        return req.json({success:false, message:"user not found"})
+    }
+    req.redirect("/")
+    req.json({success:true, userInfo:checkLogin.user, message:"user found"})
     console.log(`${username} is logging in`)
 })
 
