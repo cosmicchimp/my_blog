2f40e31c0a86b207e7b6adf37034723f0ea6b1a6
Max lambert <max.lambert429@gmail.com>
2025-06-11 19:20:04 -0600

    Signup route changes =

--- backend/routes/signupRoute.js
+++ backend/routes/signupRoute.js
@@ -7,8 +7,16 @@ const sql = neon(process.env.DATABASE_URL)
 const app = express.Router()
 app.use(express.urlencoded({ extended: true }));
 app.post("/", async (req, res) => {
+    try {
     const {username, password} = req.body
+    const crypt = await bcrypt.hash(password, 6)
+    await sql`INSERT INTO users(username, password) VALUES (${username}, ${crypt})`
     console.log(`${username} is creating an account`)
+    return res.status(500).redirect("/?signup=success")
+    }
+    catch (e) {
+        console.log("Error in signup route ", e)
+    }
 })
 
 export default app
