995add36e099b063adb0552b73bbfa3390b37fca
Max lambert <max.lambert429@gmail.com>
2025-06-14 13:32:59 -0600

    Finishing the login route

--- backend/middleware/checkLogin.js
+++ backend/middleware/checkLogin.js
@@ -8,10 +8,8 @@ const sql = neon(process.env.DATABASE_URL)
 export default async function checkLogin(username) {
   const databaseResult = await sql`SELECT * FROM users WHERE username = ${username}`
   const userData = databaseResult[0]
-
   if (!userData) {
     return { success: false } // No user found
   }
-
   return { success: true, user: userData }
 }
--- backend/routes/loginRoute.js
+++ backend/routes/loginRoute.js
@@ -3,9 +3,11 @@ import dotenv from "dotenv"
 import bcrypt from "bcrypt"
 import {neon} from "@neondatabase/serverless"
 import checkLogin from "../middleware/checkLogin.js"
+
 dotenv.config()
 const sql = neon(process.env.DATABASE_URL)
 const app = express.Router()
+ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.post("/", async (req, res) => {
     const {username, password} = req.body
