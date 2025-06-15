feec17ae83e8a6a137dd451bd2f031f4571407db
Max lambert <max.lambert429@gmail.com>
2025-06-15 16:40:11 -0600

    Creating blog creation route

--- backend/routes/blogPush.js
+++ backend/routes/blogPush.js
@@ -8,10 +8,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.post("/", async (req, res) => {
     try{
+          console.log("💥 /blogPush body:", req.body)
         const {blogContent, blogAuthor, blogTitle} = req.body
-        if (!blogContent || !blogAuthor || !blogTitle) {
-            return res.status(400).json({ success: false, message: "Missing required fields." })
-        }   
+        // if (!blogContent || !blogAuthor || !blogTitle) {
+        //     return res.status(400).json({ success: false, message: "Missing required fields." })
+        // }   
         await sql`INSERT INTO blogs (author, blog_content, blog_title) VALUES (${blogAuthor},${blogContent},${blogTitle})`
         return res.status(200).json({success:true, message:"Post successfully created"})
     }
