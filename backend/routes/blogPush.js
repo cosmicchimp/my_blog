2392baf858c1c40b18ab844874fba8c96e34e85e
Max lambert <max.lambert429@gmail.com>
2025-06-15 12:30:10 -0600

    Creating blog creation route

---
+++ backend/routes/blogPush.js
@@ -0,0 +1,23 @@
+import express from "express"
+import {neon} from "@neondatabase/serverless"
+import dotenv from "dotenv"
+dotenv.config()
+const sql = neon(process.env.DATABASE_URL);
+const app = express.Router()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.post("/", async (req, res) => {
+    try{
+        const {blogContent, blogAuthor, blogTitle} = req.body
+        if (!blogContent || !blogAuthor || !blogTitle) {
+            return res.status(400).json({ success: false, message: "Missing required fields." })
+        }
+        await sql`INSERT INTO blogs (author, blog_content, blog_title) VALUES (${blogAuthor},${blogContent},${blogTitle})`
+        return res.status(200).json({success:true, message:"Post successfully created"})
+    }
+    catch (e) {
+        console.log("Error in blogPush function: ", e)
+    }
+})
+
+export default app
--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,18 @@ import {neon} from "@neondatabase/serverless"
 import loginRoute from "./routes/loginRoute.js"
 import signupRoute from "./routes/signupRoute.js"
 import checkUsername from "./routes/checkUsername.js"
+import blogPush from "./routes/blogPush.js"
 import cors from 'cors';
 dotenv.config()
 const sql = neon(process.env.DATABASE_URL)
 const app = express()
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/login", loginRoute)
 app.use("/signup", signupRoute)
 app.use("/checkusername", checkUsername)
+app.use("/blogPush", blogPush)
 app.listen(3000, () => {
     console.log("Server Running")
 })
